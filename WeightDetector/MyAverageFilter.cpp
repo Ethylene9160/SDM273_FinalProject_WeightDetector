f14c1aac23dc223d0d55f10b6e184f2bda8ca5d6
Ethy9160 <93207157+Ethylene9160@users.noreply.github.com>
2023-05-12 18:20:54 +0800

    Add files via upload

---
+++ WeightDetector/MyAverageFilter.cpp
@@ -0,0 +1,107 @@
+#include "MyAverageFilter.h"
+#include<arduino.h>
+
+void MyAverageFilter::calculateAverage()
+{
+	average = 0;
+	for (int i = 0; i < length; ++i) {
+		average += *(my_queue + i);
+	}
+	average = average / length;
+}
+
+bool MyAverageFilter::checkBalence()
+{
+	if (isFull) {
+		calculateAverage();
+		for (int i = 0; i < length; ++i) {
+			if (!checkSingleData(*(my_queue + i))) return false;
+		}
+	}
+	return true;
+}
+
+bool MyAverageFilter::checkSingleData(mydata data)
+{
+	mydata absoluteError = abs(data-average);
+	if (absoluteError > this->absoluteError) return false;
+	if ((demical)absoluteError / (demical)average > relativeError) return false;
+	return true;
+}
+
+void MyAverageFilter::addElement(mydata data)
+{
+	*(my_queue + calculator) = data;
+	calculator = (++calculator) % length;
+}
+
+void MyAverageFilter::reset()
+{
+	isFull = false;
+	isBalence = false;
+	average = 0;
+	calculator = 0;
+	for (int i = 0; i < length; ++i) {
+		*(my_queue + i) = 0;
+	}
+}
+
+MyAverageFilter::MyAverageFilter(AverageInteface* averageInteface) :MyAverageFilter(averageInteface, _MY_AVERAGE_FILTER_)
+{
+
+}
+
+MyAverageFilter::MyAverageFilter(AverageInteface* averageInteface, mydata length, mydata absoluteError, demical relativeError)
+{
+	//todo
+
+	my_queue = new int[length];
+	this->averageInteface = averageInteface;
+
+	this->length = length;
+	this->absoluteError = absoluteError;
+	this->relativeError = relativeError;
+
+
+
+	this->averageInteface->show(111);
+	reset();
+}
+
+void MyAverageFilter::update(mydata data)
+{
+	if (isBalence) {
+		if (checkSingleData(data)) {
+			averageInteface->show(average);
+			return;
+		}
+		else {
+		}
+		reset();
+	}
+	else {
+
+		if (isFull) {
+			if (checkBalence()) {
+				isBalence = true;
+			}
+		}
+		else {
+			if (calculator + 1 == this->length) {
+				isFull = true;
+			}
+		}
+	}
+
+	this->addElement(data);
+	averageInteface->show(data);
+}
+
+MyAverageFilter::MyAverageFilter(AverageInteface* averageInteface, mydata length) :MyAverageFilter(averageInteface, length, 10, 0.05)
+{
+
+}
+
+//mydata MyAverageFilter::abs(mydata data){
+//  return data > 0? data:-data;
+//}
