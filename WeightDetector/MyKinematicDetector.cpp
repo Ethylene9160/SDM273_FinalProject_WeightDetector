b0f689aa7576dcebf584d8965f2f4638fb9c274b
Ethy9160 <93207157+Ethylene9160@users.noreply.github.com>
2023-05-12 18:23:46 +0800

    Add files via upload

---
+++ WeightDetector/MyKinematicDetector.cpp
@@ -0,0 +1,259 @@
+#include "MyKinematicDetector.h"
+
+void MyKinematicDetector::begin()
+{
+	//Serial.begin(9600); 
+	Wire.begin(); 
+	writeMPUReg(0x6B, 0); 
+
+	calibration();
+	nLastTime = micros(); //record current time 
+}
+
+void MyKinematicDetector::writeMPUReg(int nReg, unsigned char nVal)
+{
+	Wire.beginTransmission(MPU);
+	Wire.write(nReg);
+	Wire.write(nVal);
+	Wire.endTransmission(true);
+}
+
+unsigned char MyKinematicDetector::readMPUReg(int nReg)
+{
+	Wire.beginTransmission(MPU);
+	Wire.write(nReg);
+	Wire.requestFrom(MPU, 1, true);
+	Wire.endTransmission(true);
+	return Wire.read();
+}
+
+void MyKinematicDetector::readAccGyr(int* pVals)
+{
+	Wire.beginTransmission(MPU);
+	Wire.write(0x3B);
+	Wire.requestFrom(MPU, nValCnt * 2, true);
+	Wire.endTransmission(true);
+	for (long i = 0; i < nValCnt; ++i) {
+		pVals[i] = Wire.read() << 8 | Wire.read();
+	}
+}
+
+void MyKinematicDetector::calibration()
+{
+	demical valSums[7] = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0 };
+	//�����
+	for (int i = 0; i < nCalibTimes; ++i) {
+		int mpuVals[nValCnt];
+		readAccGyr(mpuVals);
+		for (int j = 0; j < nValCnt; ++j) {
+			valSums[j] += mpuVals[j];
+		}
+	}
+	//����ƽ��
+	for (int i = 0; i < nValCnt; ++i) {
+		calibData[i] = int(valSums[i] / nCalibTimes);
+	}
+	calibData[2] += 16384; //��оƬZ����ֱ���£��趨��̬�����㡣
+}
+
+demical MyKinematicDetector::getRoll(demical* pRealVals, demical fNorm)
+{
+	demical fNormXZ = sqrt(pRealVals[0] * pRealVals[0] + pRealVals[2] * pRealVals[2]);
+	demical fCos = fNormXZ / fNorm;
+	return acos(fCos) * fRad2Deg;
+}
+
+demical MyKinematicDetector::getYaw(demical* pRealVals, demical fNorm)
+{
+	demical fNormXY = sqrt(pRealVals[0] * pRealVals[0] + pRealVals[1] * pRealVals[1]);
+	demical fCos = fNormXY / fNorm;
+	return acos(fCos) * fRad2Deg;
+}
+
+void MyKinematicDetector::rectify(int* pReadout, demical* pRealVals)
+{
+	for (int i = 0; i < 3; ++i) {
+		pRealVals[i] = (demical)(pReadout[i] - calibData[i]) / 16384.0f;
+	}
+	pRealVals[3] = pReadout[3] / 340.0f + 36.53;
+	for (int i = 4; i < 7; ++i) {
+		pRealVals[i] = (demical)(pReadout[i] - calibData[i]) / 131.0f;
+	}
+}
+
+demical MyKinematicDetector ::getPitch(demical* pRealVals, demical fNorm) {
+	demical fNormYZ = sqrt(pRealVals[1] * pRealVals[1] + pRealVals[2] * pRealVals[2]);
+	demical fCos = fNormYZ / fNorm;
+	return acos(fCos) * fRad2Deg;
+}
+
+
+
+MyKinematicDetector::MyKinematicDetector()
+{
+	//(*this).begin();
+  this->myFilter = new MyAverageFilter(this, 10, 10, 0.05);
+}
+
+void MyKinematicDetector::mainLoop()
+{
+	int readouts[nValCnt];
+	readAccGyr(readouts); 
+
+	demical realVals[7];
+	rectify(readouts, realVals); 
+
+	
+	demical fNorm = sqrt(realVals[0] * realVals[0] + realVals[1] * realVals[1] + realVals[2] * realVals[2]);
+  Serial.print("fNorm: ");
+  Serial.println(fNorm);
+	demical fRoll = getRoll(realVals, fNorm); 
+	if (realVals[1] > 0) {
+		fRoll = -fRoll;
+	}
+	demical fPitch = getPitch(realVals, fNorm); 
+	if (realVals[0] < 0) {
+		fPitch = -fPitch;
+	}
+
+	unsigned long nCurTime = micros();
+	demical dt = (double)(nCurTime - nLastTime) / 1000000.0;
+	
+	demical fNewRoll = kalmanRoll.getAngle(fRoll, realVals[4], dt);
+	demical fNewPitch = kalmanPitch.getAngle(fPitch, realVals[5], dt);
+	
+	demical fRollRate = (fNewRoll - fLastRoll) / dt;
+	demical fPitchRate = (fNewPitch - fLastPitch) / dt;
+
+	fLastRoll = fNewRoll;
+	fLastPitch = fNewPitch;
+	nLastTime = nCurTime;
+
+	Serial.print("Roll:");
+	Serial.print(fNewRoll); Serial.print('(');
+	Serial.print(fRollRate); Serial.print("),\tPitch:");
+	Serial.print(fNewPitch); Serial.print('(');
+	Serial.print(fPitchRate); Serial.print(")\n");
+	//delay(10);
+}
+
+DataStorager* MyKinematicDetector::beforeRead()
+{
+  int readouts[nValCnt];
+  readAccGyr(readouts);
+
+  demical realVals[7];
+  rectify(readouts, realVals);
+  demical fNorm = sqrt(realVals[0] * realVals[0] + realVals[1] * realVals[1] + realVals[2] * realVals[2]);
+  return new DataStorager(realVals, fNorm);
+}
+
+demical MyKinematicDetector::getRoll()
+{
+  DataStorager* storager = beforeRead();
+  demical rv = *(storager->getVals());
+  demical fNorm = storager->getNorm();
+  delete storager;
+  return this->getRoll(&rv, fNorm);
+}
+
+demical MyKinematicDetector::getPitch()
+{
+  /*
+  DataStorager* storager = beforeRead();
+  
+  demical fPitch = this->getPitch(storager->getVals(), storager->getNorm());
+  
+  if (*(storager->getVals()) < 0) {
+    fPitch = -fPitch;
+  }
+  unsigned long nCurTime = micros();
+  demical dt = (double)(nCurTime - nLastTime) / 1000000.0;
+  
+  demical fNewPitch = kalmanPitch.getAngle(fPitch, storager->getVals()[5], dt);
+  
+  demical fPitchRate = (fNewPitch - fLastPitch) / dt;
+
+  fLastPitch = fNewPitch;
+  nLastTime = nCurTime;
+  
+  delete storager;
+  return fNewPitch;
+  */
+
+
+  int readouts[nValCnt];
+  readAccGyr(readouts); 
+
+  demical realVals[7];
+  rectify(readouts, realVals); 
+
+  
+  demical fNorm = sqrt(realVals[0] * realVals[0] + realVals[1] * realVals[1] + realVals[2] * realVals[2]);
+
+  demical fPitch = getPitch(realVals, fNorm); 
+  if (realVals[0] < 0) {
+    fPitch = -fPitch;
+  }
+  //Serial.print("unFilt:");
+  //Serial.println(fPitch);
+  unsigned long nCurTime = micros();
+  demical dt = (double)(nCurTime - nLastTime) / 1000000.0;
+  
+//  demical fNewPitch = kalmanPitch.getAngle(fPitch, realVals[5], dt);
+
+//  Serial.print("angle: ");
+  //demical d1 = dt*realVals[5];
+  //finalPitch += d1;
+  //Serial.println(finalPitch);
+  demical fNewPitch = fPitch*filterCoeffecient + (1.f-filterCoeffecient)*fLastPitch;
+//  demical d2 = fPitch*filterCoeffecient + (1.f-filterCoeffecient)*fLastPitch;
+  //Serial.print("first_order:");
+  //Serial.println(ToWeightData::analog2digit(d2, 0));
+  myFilter->update(ToWeightData::analog2digit(fNewPitch, 0));
+//  demical fRollRate = (fNewRoll - fLastRoll) / dt;
+  demical fPitchRate = (fNewPitch - fLastPitch) / dt;
+
+//  fLastRoll = fNewRoll;
+  fLastPitch = fNewPitch;
+//fLastPitch = d2;
+  nLastTime = nCurTime;
+  return fNewPitch;
+  
+}
+
+demical MyKinematicDetector::getYaw()
+{
+  DataStorager* storager = beforeRead();
+  demical rv = *(storager->getVals());
+  demical fNorm = storager->getNorm();
+  delete storager;
+  
+
+  return this->getYaw(&rv, fNorm);
+}
+
+DataStorager::DataStorager(demical* realVals, demical fNorm)
+{
+  this->realVals = realVals;
+  this->fNorm = fNorm;
+}
+
+demical* DataStorager::getVals()
+{
+  return this->realVals;
+}
+
+demical DataStorager::getNorm()
+{
+  return this->fNorm;
+}
+
+demical MyKinematicDetector::getAngle(){
+  return this->getPitch();
+}
+
+void MyKinematicDetector::show(mydata d){
+  Serial.print("my_average_filter: ");
+  Serial.println(d);
+}
---
+++ WeightDetector/MyKinematicDetector.h
@@ -0,0 +1,113 @@
+//#pragma once
+#ifndef MY_KINEMATICS_DETECTOR
+#define MY_KINEMATICS_DETECTOR 3
+#include<Arduino.h>
+#include <Wire.h>
+#include <Math.h>
+
+#ifndef MY_DEMICAL
+#define MY_DEMICAL
+typedef float demical;
+#endif
+
+#include "Kalman.h"
+#include "DataInterface.h"
+#include "ToWeightData.h"
+#include "MyAverageFilter.h"
+//typedef int int16_t;//remenber to delete this!
+
+class DataStorager{
+private:
+  demical* realVals;
+  demical fNorm;
+public:
+  DataStorager(demical* realVals, demical fNorm);
+  
+  demical* getVals();
+
+  demical getNorm();
+};
+class MyKinematicDetector:public AngleDetector, public AverageInteface
+{
+private:
+	const demical fRad2Deg = 57.295779513f; //rad to degree
+	static const int MPU = 0x68; //MPU-6050 I2C addressַ
+	static const int nValCnt = 7; //the number of the register once read
+  static const demical filterCoeffecient = 0.15;//coeffecient of 1 order filtering
+  MyAverageFilter* myFilter;//delete this
+  
+	const int nCalibTimes = 1000; //clear time
+	int calibData[nValCnt]; //clear data
+
+	unsigned long nLastTime = 0; //last reading time
+	demical fLastRoll = 0.0f; //last Roll
+	demical fLastPitch = 0.0f; //last pitch
+	Kalman kalmanRoll; //Roll filter
+	Kalman kalmanPitch; //Pitch filter
+
+  
+  demical finalPitch;//remenber to detele this
+	
+
+	/*
+	*��MPU6050д��һ���ֽڵ�����
+	* ָ���Ĵ�����ַ��һ���ֽڵ�ֵ
+	*/
+	void writeMPUReg(int nReg, unsigned char nVal);
+
+	/* ��MPU6050�������ٶȼ������������¶Ⱥ��������ٶȼ�
+	* ������ָ����������
+	*/
+	void readAccGyr(int* pVals);
+
+	/*�Դ�����������ͳ�ƣ�У׼ƽ��ƫ����*/
+	void calibration();
+
+	/*��MPU6050����һ���ֽڵ�����
+	* ָ���Ĵ�����ַ�����ض�����ֵ
+	*/
+	unsigned char readMPUReg(int nReg);
+
+	/*���Roll��*/
+	demical getRoll(demical* pRealVals, demical fNorm);
+
+	/*���Pitch��*/
+	demical getPitch(demical* pRealVals, demical fNorm);
+
+	/*���Yaw��*/
+	demical getYaw(demical* pRealVals, demical fNorm);
+
+	/*�Զ������о���������ƫ�ƣ���ת��Ϊ��������*/
+	void rectify(int* pReadout, demical* pRealVals);
+  
+  DataStorager* beforeRead();
+public:
+	/*
+	constructor.
+	Ples initialize it in <code>void setup()</code> function,
+	use keyword <code>new</code> to establish this in the stack space, 
+	will be a better choice.
+	*/
+  /*
+  a init function for initializing this class, with MPU8265 inside.
+  Do not use it.
+  */
+  void begin();
+  
+	MyKinematicDetector();
+
+	void mainLoop();
+  
+  demical getRoll();
+
+  demical getPitch();
+
+  demical getYaw();
+
+  virtual demical getAngle();
+
+  virtual void show(mydata data);
+};
+
+
+#endif
